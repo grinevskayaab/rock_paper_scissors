d10c96880954778ea886ee0951e0d5f84dc65d85
grinevskaya_ab <voronkova_ab@mail.ru>
2024-05-08 18:56:25 +0500

    Убрала лишний класс

--- src/main/java/com/githab/grinevskayaab/ApplicationContext.java
+++ src/main/java/com/githab/grinevskayaab/ApplicationContext.java
@@ -2,28 +2,24 @@ package com.githab.grinevskayaab;
 
 import com.githab.grinevskayaab.game.GameMenu;
 import com.githab.grinevskayaab.game.GameRockPaperScissors;
-import com.githab.grinevskayaab.game.GameStartAction;
 import com.githab.grinevskayaab.gameOptions.GameOptions;
-import com.githab.grinevskayaab.gameOptions.GameOptionsConsole;
 import com.githab.grinevskayaab.gameOptions.GameOptionsTest;
 import com.githab.grinevskayaab.leaderboard.FileLeaderboard;
 import com.githab.grinevskayaab.leaderboard.Leaderboard;
 
 
 public class ApplicationContext {
-
-    private final GameStartAction startAction;
+    private final GameMenu gameMenu;
 
     public ApplicationContext() {
         Leaderboard leaderboard = new FileLeaderboard();
 //        GameOptions gameOptions = new GameOptionsConsole();
         GameOptions gameOptions = new GameOptionsTest();
-        GameMenu gameMenu = new GameMenu();
         GameRockPaperScissors game = new GameRockPaperScissors(gameOptions);
-        this.startAction = new GameStartAction(game, leaderboard, gameMenu);
+        this.gameMenu = new GameMenu(game, leaderboard);
     }
 
     public void run() {
-        startAction.run();
+        gameMenu.run();
     }
 }
--- src/main/java/com/githab/grinevskayaab/game/GameMenu.java
+++ src/main/java/com/githab/grinevskayaab/game/GameMenu.java
@@ -1,11 +1,30 @@
 package com.githab.grinevskayaab.game;
 
+import com.githab.grinevskayaab.leaderboard.Leaderboard;
+
 import java.util.Scanner;
 
 public class GameMenu {
     private final Scanner scanner = new Scanner(System.in);
+    private final GameRockPaperScissors game;
+    private final Leaderboard leaderboard;
 
-    public Integer getAction() {
+    public GameMenu(GameRockPaperScissors game, Leaderboard leaderboard) {
+        this.game = game;
+        this.leaderboard = leaderboard;
+    }
+
+    public void run() {
+        Integer gameAction = getAction(); // 1 - играть, 2 - Таблица
+        if(gameAction == 2) {
+            String leaderboardString = leaderboard.getLeaderboard();
+            System.out.println(leaderboardString);
+        } else {
+            game.start();
+        }
+    }
+
+    private Integer getAction() {
         System.out.println("Выберите пункт:\n 1 - играть\n 2 - Показать таблицу со счетом игроков");
         return scanner.nextInt();
     }
